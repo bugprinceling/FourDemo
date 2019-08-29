205980653979db25d4eabc859cb0f7e4b032f2bb
libai <chuangqianmingyue@163.com>
2019-08-29 19:09:54 +0800

    使用idea第一次修改然后提交

--- src/com/gittest/Main.java
+++ src/com/gittest/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("第一次创建项目 使用git提交到本地仓库");
+        System.out.println("第一次修改项目 使用idea提交到本地仓库");
     }
 
 
