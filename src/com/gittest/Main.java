3c4d5db8b3717f99148b0963635c99058edad898
libai <chuangqianmingyue@163.com>
2019-08-29 19:28:39 +0800

    第二次修改使用git提交

--- src/com/gittest/Main.java
+++ src/com/gittest/Main.java
@@ -1,10 +1,11 @@
-package com.gittest;
+﻿package com.gittest;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("第一次创建项目 使用git提交到本地仓库");
         System.out.println("第一次修改项目 使用idea提交到本地仓库");
+        System.out.println("第二次修改项目 使用git提交到本地仓库");
     }
 
 
